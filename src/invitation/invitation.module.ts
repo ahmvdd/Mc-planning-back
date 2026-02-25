fdea78b2a51ce57b7f60340e5c0e18840d959dec
ahmed <112165741+ahmvdd@users.noreply.github.com>
2026-02-26 00:01:14 +0100

    Add AuthModule to InvitationModule imports

--- src/invitation/invitation.module.ts
+++ src/invitation/invitation.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { InvitationController } from './invitation.controller';
 import { InvitationService } from './invitation.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [InvitationController],
-  providers: [InvitationService],
+    imports: [PrismaModule, AuthModule],
+    controllers: [InvitationController],
+    providers: [InvitationService],
 })
-export class InvitationModule {}
+  export class InvitationModule {}
